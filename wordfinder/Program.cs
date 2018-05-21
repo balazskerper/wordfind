f97f41e9b113341b0206c86073fa923365dd6972
balazskerper <balazs.kerper@gmail.com>
2018-05-21 19:36:36 +0200

    test changes

--- wordfinder/Program.cs
+++ wordfinder/Program.cs
@@ -7,6 +7,7 @@ namespace wordfinder
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
