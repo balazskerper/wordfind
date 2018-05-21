090fb84ff613ab7b65d83f1a18311c0825672cc4
balazskerper <balazs.kerper@gmail.com>
2018-05-21 20:45:06 +0200

    added code to read the file with words

--- wordfinder/Program.cs
+++ wordfinder/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace wordfinder
 {
@@ -6,8 +11,33 @@ namespace wordfinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            ConcurrentDictionary<string, List<LetterCount>> LetterCountsForWords = new ConcurrentDictionary<string, List<LetterCount>>();
+
+            string WordsFilePath = @"/Users/balazskerper/Projects/test_data/words_alpha.txt";
+
+            List<string> words = File.ReadAllLines(WordsFilePath).ToList();
+
+            Parallel.ForEach(words, (word) =>
+            {
+                LetterCountsForWords.AddOrUpdate(word, GetLetterCountsForWord(word), (Key, Value) => Value);
+            });
+
+            Console.WriteLine(words.Count);
+            Console.WriteLine(LetterCountsForWords.Count);
+        }
+        public static List<LetterCount> GetLetterCountsForWord(string word)
+        {
+            List<LetterCount> letterCounts = new List<LetterCount>();
+            //TODO: create logic for getting LetterCounts
+            return letterCounts;
         }
     }
+
+    struct LetterCount
+    {
+        char Char { get; set; }
+        byte Count { get; set; }
+    }
+
+
 }
